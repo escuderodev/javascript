166e48c7d0d4ac4ae7133117d38e2c68038847b9
EDUARDO ESCUDERO <88359144+escuderodev@users.noreply.github.com>
2023-01-10 16:43:45 -0300

    att

--- orientação_a_objetos/byte_bank/main.js
+++ orientação_a_objetos/byte_bank/main.js
@@ -1,29 +1,38 @@
 // importando classes
+import { Cliente } from "./models/Cliente"
+import { ContaCorrente } from "./models/ContaCorrente"
+
+//utilizando as clases importadas
+const primeiroCliente = new Cliente();
+primeiroCliente.nome = "Eduardo Escudero";
+primeiroCliente.cpf = "33188944318";
+
+console.log(`Cliente: ${primeiroCliente.nome} CPF: ${primeiroCliente.cpf}`);
 
 
 
-const primeiraConta = new ContaCorrente();
-primeiraConta.agencia = '1012';
-primeiraConta.numero = '850408';
-primeiraConta.deposita(1000);
+// const primeiraConta = new ContaCorrente();
+// primeiraConta.agencia = '1012';
+// primeiraConta.numero = '850408';
+// primeiraConta.deposita(1000);
 
-const segundaConta = new ContaCorrente();
-segundaConta.agencia = '8541';
-segundaConta.numero = '584712';
-segundaConta.deposita(1000);
+// const segundaConta = new ContaCorrente();
+// segundaConta.agencia = '8541';
+// segundaConta.numero = '584712';
+// segundaConta.deposita(1000);
 
-console.log(`Primeira Conta R$: ${primeiraConta.getsaldo()}`);
-console.log(`Segunda Conta R$: ${segundaConta.getsaldo()}\n`);
+// console.log(`Primeira Conta R$: ${primeiraConta.getsaldo()}`);
+// console.log(`Segunda Conta R$: ${segundaConta.getsaldo()}\n`);
 
-primeiraConta.saca(50);
-segundaConta.deposita(50);
+// primeiraConta.saca(50);
+// segundaConta.deposita(50);
 
-console.log(`Primeira Conta R$: ${primeiraConta.getsaldo()}`);
-console.log(`Segunda Conta R$: ${segundaConta.getsaldo()}\n`);
+// console.log(`Primeira Conta R$: ${primeiraConta.getsaldo()}`);
+// console.log(`Segunda Conta R$: ${segundaConta.getsaldo()}\n`);
 
-segundaConta.transfere(50, primeiraConta);
+// segundaConta.transfere(50, primeiraConta);
 
-console.log(`Primeira Conta R$: ${primeiraConta.getsaldo()}`);
-console.log(`Segunda Conta R$: ${segundaConta.getsaldo()}\n`);
+// console.log(`Primeira Conta R$: ${primeiraConta.getsaldo()}`);
+// console.log(`Segunda Conta R$: ${segundaConta.getsaldo()}\n`);
 
-primeiraConta.deposita(-100);
+// primeiraConta.deposita(-100);
--- orientação_a_objetos/byte_bank/models/contaCorrente.js
+++ orientação_a_objetos/byte_bank/models/contaCorrente.js
@@ -1,4 +1,4 @@
-class ContaCorrente {
+export class ContaCorrente {
     agencia;
     numero;
     _saldo = 0;
