b174cafea93fabe284f780b19818c0dd7be8f6e8
EDUARDO ESCUDERO <88359144+escuderodev@users.noreply.github.com>
2023-01-14 11:30:08 -0300

    att

--- orientação_a_objetos/byte_bank/main.js
+++ orientação_a_objetos/byte_bank/main.js
@@ -1,19 +1,14 @@
 // realizando importações
 import {Cliente} from "./models/Cliente.js"
 import {ContaCorrente} from "./models/ContaCorrente.js"
+import {ContaPoupanca} from "./models/ContaPoupanca.js";
 
 const cliente1 = new Cliente("Eduardo", 33188942818);
-const cliente2 = new Cliente("Carol", 33104296871);
 
-const contaCorrenteEduardo = new ContaCorrente(1012, cliente1);
-const contaCorrenteCarol = new ContaCorrente(4432, cliente2);
+const contaCorrente = new ContaCorrente(1012, cliente1);
+const contaPoupanca = new ContaPoupanca(50, cliente1);
 
-contaCorrenteEduardo.deposita(1000);
-contaCorrenteCarol.deposita(1000);
+contaCorrente.deposita(1000);
 
-let valor = 200;
-contaCorrenteCarol.transfere(valor, contaCorrenteEduardo);
-
-console.log(contaCorrenteEduardo.saldo);
-console.log(contaCorrenteCarol.saldo);
-
+console.log(`Conta Corrente - Saldo R$: ${contaCorrente.saldo}`);
+console.log(`Conta Poupanca - Saldo R$: ${contaPoupanca.saldo}`);
--- orientação_a_objetos/byte_bank/models/ContaPoupanca.js
+++ orientação_a_objetos/byte_bank/models/ContaPoupanca.js
@@ -4,4 +4,64 @@ export class ContaPoupanca {
         this._cliente = cliente;
         this._agencia = agencia;
     }
+
+     // métodos de regra de negócio
+     deposita(valor) {
+        if(valor > 0) {
+            this._saldo += valor;
+            console.log(`Depósito de R$ ${valor.toFixed(2)} realizado com sucesso!`);
+        } else {
+            console.log('Depósito não realizado! O valor deve ser maior que R$ 0,00.');
+        };
+    };
+
+    saca(valor) {
+        if(this._saldo >= valor) {
+            console.log(`Saque de ${valor.toFixed(2)} realizado com sucesso!`);
+            this._saldo -= valor;
+        } else {
+            console.log('Saldo insuficiente!');
+        };
+    };
+
+    transfere(valor, outraContra) {
+        if(this._saldo >= valor) {
+            console.log(`Transferência de ${valor.toFixed(2)} realizada com sucesso!`);
+            this._saldo -= valor;
+            outraContra.deposita(valor);
+        } else {
+            console.log('Saldo insuficiente!');
+        };
+    };
+
+    // getters e setters
+    get agencia() {
+        return this._agencia;
+    };
+
+    set agencia(agencia) {
+        this._agencia = agencia;
+    };
+
+    get numero() {
+        return this._numero;
+    };
+
+    set numero(numero) {
+        this._numero = numero;
+    };
+
+    get saldo() {
+        return this._saldo.toFixed(2);
+    };
+
+    get titular() {
+        return this._titular;
+    };
+
+    set titular(novoCliente) {
+        if (novoCliente instanceof Cliente) {
+            this._titular = novoCliente;
+        }
+    };
 }
