55c673b5d45145398096c228edcd206061eb7436
EDUARDO ESCUDERO <88359144+escuderodev@users.noreply.github.com>
2023-01-14 12:13:41 -0300

    oo em javascript

--- orientação_a_objetos/byte_bank/models/ContaCorrente.js
+++ orientação_a_objetos/byte_bank/models/ContaCorrente.js
@@ -1,77 +1,14 @@
 import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js";
 
-export class ContaCorrente {
-    _agencia;
-    _numero;
-    _titular;
-    _saldo = 0;
+export class ContaCorrente extends Conta {
     static numeroDeContas = 0;
 
     constructor(agencia, numero, titular) {
-        this._agencia = agencia;
-        this._numero = numero;
-        this.titular = titular;
-        this._saldo = 0;
+        super(agencia, numero, titular, 0);
         ContaCorrente.numeroDeContas += 1;
     }
 
     // métodos de regra de negócio
-    deposita(valor) {
-        if(valor > 0) {
-            this._saldo += valor;
-            console.log(`Depósito de R$ ${valor.toFixed(2)} realizado com sucesso!`);
-        } else {
-            console.log('Depósito não realizado! O valor deve ser maior que R$ 0,00.');
-        };
-    };
 
-    saca(valor) {
-        if(this._saldo >= valor) {
-            console.log(`Saque de ${valor.toFixed(2)} realizado com sucesso!`);
-            this._saldo -= valor;
-        } else {
-            console.log('Saldo insuficiente!');
-        };
-    };
-
-    transfere(valor, outraContra) {
-        if(this._saldo >= valor) {
-            console.log(`Transferência de ${valor.toFixed(2)} realizada com sucesso!`);
-            this._saldo -= valor;
-            outraContra.deposita(valor);
-        } else {
-            console.log('Saldo insuficiente!');
-        };
-    };
-
-    // getters e setters
-    get agencia() {
-        return this._agencia;
-    };
-
-    set agencia(agencia) {
-        this._agencia = agencia;
-    };
-
-    get numero() {
-        return this._numero;
-    };
-
-    set numero(numero) {
-        this._numero = numero;
-    };
-
-    get saldo() {
-        return this._saldo.toFixed(2);
-    };
-
-    get titular() {
-        return this._titular;
-    };
-
-    set titular(novoCliente) {
-        if (novoCliente instanceof Cliente) {
-            this._titular = novoCliente;
-        }
-    };
 };
